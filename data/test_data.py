2b96716fe81b1cb01d5c965b2591f17c9d678708
AZANIR <AZANIR@users.noreply.github.com>
2022-12-04 15:21:46 +0000

    Automated deployment: Sun Dec  4 15:21:46 UTC 2022 e33a6cbf680344ae23e01a315d6d8646495eab11

--- data/test_data.py
+++
@@ -1,9 +0,0 @@
-
-
-class Data:
-
-    colors = ["Green", "Blue", "Black", "Red"]
-    username = "test_user"
-    email = "email@gmail.com"
-    current_address = "Test_Current_Address"
-    permanent_address = "Test_Permanent_Address"
